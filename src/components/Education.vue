e5a64e8052e81b75ffa51b23076967a3f0ddc798
SiphoChris <siphonkebe@gmail.com>
2024-07-03 11:22:06 +0200

    creation of the education component

---
+++ src/components/Education.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <div v-for="education in education" :key="education.id">
+        <p>{{ education.year }}: {{ education.description }} at {{ education.place }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { mapState } from 'vuex';
+  
+  export default {
+    name: 'EducationComp',
+    computed: {
+      ...mapState(['education']),
+    },
+  };
+  </script>
