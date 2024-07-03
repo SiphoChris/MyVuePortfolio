614294b8b55c1e6686dfa44deb2ac6365b5b56e0
SiphoChris <siphonkebe@gmail.com>
2024-07-03 09:51:32 +0200

    creation of the navigation bar, and also its styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
-  
+    <HomeView />
 </template>
+
+<script>
+import HomeView from '@/views/HomeView.vue'
+export default {
+components: {
+  HomeView
+}
+}
+</script>
 
 <style src="./assets/css/style.css">
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
+    <Nav />
   </div>
 </template>
 
 <script>
+import Nav from '@/components/NavBar.vue'
 
 export default {
   name: 'HomeView',
   components: {
+    Nav
   }
 }
 </script>
