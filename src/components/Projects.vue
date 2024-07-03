6082eb6f3ee4e499e4b0886fd3a86421d58e0612
SiphoChris <siphonkebe@gmail.com>
2024-07-03 11:30:17 +0200

    creation of the projects component

---
+++ src/components/Projects.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <div v-for="project in projects" :key="project.name">
+        <h3>{{ project.name }}</h3>
+        <p>{{ project.description }}</p>
+        <a :href="project.gitHub">GitHub</a>
+        <a :href="project.vercel">Live Demo</a>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { mapState } from 'vuex';
+  
+  export default {
+    name: 'ProjectsComp',
+    computed: {
+      ...mapState(['projects']),
+    },
+  };
+  </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <Nav />
     <Hero />
     <Education />
+    <Projects />
     <FooterComp />
   </div>
 </template>
@@ -11,6 +12,7 @@
 import Nav from '@/components/NavBar.vue'
 import Hero from '@/components/HeroSection.vue'
 import Education from '@/components/Education.vue'
+import Projects from '@/components/Projects.vue'
 import FooterComp from '@/components/Footer.vue'
 
 export default {
@@ -19,6 +21,7 @@ export default {
     Nav,
     Hero,
     Education,
+    Projects,
     FooterComp,
   }
 }
