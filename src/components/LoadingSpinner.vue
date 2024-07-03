f140adbe1c3538db8a0b5e20baca2c4a72f28d83
SiphoChris <siphonkebe@gmail.com>
2024-07-03 14:34:33 +0200

    basic styling

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,26 @@
+<!-- Spinner.vue -->
+<template>
+    <div v-if="loading" class="spinner-container">
+      <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      loading: Boolean
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .spinner-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  </style>
+  
