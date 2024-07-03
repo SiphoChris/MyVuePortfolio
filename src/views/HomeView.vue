ed12387cb072ff4eb096e485b6f0a3657834c4df
SiphoChris <siphonkebe@gmail.com>
2024-07-03 11:46:32 +0200

    creation of the container to define the overall layout of the main section of the page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,21 @@
 <template>
   <div class="home">
     <Nav />
-    <Hero />
-    <Education />
-    <Projects />
+    <MainSection />
     <FooterComp />
   </div>
 </template>
 
 <script>
 import Nav from '@/components/NavBar.vue'
-import Hero from '@/components/HeroSection.vue'
-import Education from '@/components/Education.vue'
-import Projects from '@/components/Projects.vue'
+import MainSection from '@/components/MainSection.vue'
 import FooterComp from '@/components/Footer.vue'
 
 export default {
   name: 'HomeView',
   components: {
     Nav,
-    Hero,
-    Education,
-    Projects,
+    MainSection,
     FooterComp,
   }
 }
